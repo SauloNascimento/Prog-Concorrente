97fe29c33f075c2a00ba76164f11e4b17b82c815
tainahemmanuele <tainahemmanuele@gmail.com>
2019-05-14 10:24:59 -0300

    Código questão 1 pronto. Feito por Tainah, Saulo e João

--- lista1/questao1/questao1.c
+++ lista1/questao1/questao1.c
@@ -3,17 +3,20 @@
 #include <pthread.h>
 
 long int counter = 0;
+pthread_mutex_t mutex;
 
 void* run(void* args){
 	int my_id;
 	long int j;
 	
 	my_id = (int) args;
+	pthread_mutex_lock(&mutex);
 	
 	for(j = 0; j < 1e7 ; j++){
 		counter = counter + 1;
 	}
 	
+	pthread_mutex_unlock(&mutex);
 	printf("my_id=%d j=%ld counter=%ld\n", my_id , j , counter);
 	pthread_exit(my_id);
 	
@@ -23,6 +26,7 @@ int main (int argc , char *argv[]){
 	
 	int i;
 	pthread_t pthreads[3];
+	pthread_mutex_init(&mutex, NULL);
 	
 	for(i = 0; i < 3 ; i++){
 		pthread_create(&pthreads[i], NULL, &run, (void*) i);
