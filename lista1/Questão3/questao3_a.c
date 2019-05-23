83a26bf1a14175ce2816a6be49e4398af04228e9
tainahemmanuele <tainahemmanuele@gmail.com>
2019-05-23 17:03:45 -0300

    Mais alteterações. Necessita de correções

--- lista1/Questão3/questao3_a.c
+++ lista1/Questão3/questao3_a.c
@@ -3,23 +3,32 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <time.h>
 
-
+long int counter = 0;
 pthread_barrier_t barreira;
 
 void* run(void* args){
 	int thread_id;
 	
 	thread_id = (int) args;
-	request();
-	printf("A thread que terminou de executar primeiro foi a thread: %d", thread_id);
+	int num = request();
+	counter++;
+	
+	if (counter == 1) {
+		printf("A thread que terminou de executar primeiro foi a thread: %d\n", thread_id);
+	}
 	pthread_barrier_destroy(&barreira);
+	
+
+
+	return NULL;
+
 	
 }
 
 int request(){
-	int num = 1 + (rand() % 30 );
-	printf("O tempo é de %d", num);
+	int num = 1 + rand() % (30 -1);
 	sleep(num);
 	return num;
 }
@@ -29,20 +38,26 @@ int request(){
 int gateway(int num_replicas){
 	int i;
 	pthread_t pthreads[num_replicas];
-	pthread_barrier_init(&barreira, NULL,num_replicas+1);
+	pthread_barrier_init(&barreira, NULL, 1);
 	
 	for(i = 0; i < num_replicas; i++){
-		pthread_create(&pthreads[i], NULL, &run,(void*) i);
+		pthread_create(&pthreads[i], NULL, &run, (void*) i);
 	}
 	
+	pthread_barrier_wait(&barreira);
+
 
 	for(i = 0; i < num_replicas ; i++){
 		pthread_join(pthreads[i], NULL);
 	}
+	
+	pthread_barrier_destroy(&barreira);
+	
+	return 0;
 }
 
 int main (int argc , char *argv[]){
-	int value = 6;
+	int value = 40;
 	gateway(value);
 	
 }
