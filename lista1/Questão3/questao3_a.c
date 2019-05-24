4fc33f24d15f97f5bb3b9b55d6c5b0434d87152e
tainahemmanuele <tainahemmanuele@gmail.com>
2019-05-24 11:07:35 -0300

    Algumas possiveis soluções - Tainah , Saulo e João

--- lista1/Questão3/questao3_a.c
+++ lista1/Questão3/questao3_a.c
@@ -4,57 +4,53 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
+#include <semaphore.h>
 
 long int counter = 0;
-pthread_barrier_t barreira;
+pthread_mutex_t mutex;
+pthread_cond_t  cond;
+sem_t sema;
+int retorno;
 
+void* run(void* args){
+	int thread_id;
+	long int j;
+	
+	thread_id = (int) args;
+	pthread_mutex_lock(&mutex);
+	int num = request();
+	pthread_mutex_unlock(&mutex);
 
+	printf("A thread que terminou de executar primeiro foi a thread: %d\n", num);
+	printf("O número sorteado por request foi: %d\n" , num);
+
+	pthread_exit(thread_id);
+	
+}
 
 int request(){
+	pthread_cond_wait(&cond);
 	int num = 1 + rand() % (30 -1);
 	sleep(num);
-	return num;
+	sem_post(&sema);
+	pthread_exit(num);
 }
 
 int gateway(int num_replicas){
-	
-	void* run(void* args){
-	int thread_id;
-	
-	thread_id = (int) args;
-	int num = request();
-	counter++;
-	
-	if (counter == 1) {
-		printf("A thread que terminou de executar primeiro foi a thread: %d\n", thread_id);
-		printf("O número sorteado por request foi: %d\n" , num);
-	}
-	pthread_barrier_destroy(&barreira);
-	
-
-
-	return NULL;
-
-	
-	}
 	int i;
 	pthread_t pthreads[num_replicas];
-	pthread_barrier_init(&barreira, NULL, 1);
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&cond, NULL);
+	sem_init(&mutex, 0, 0);
 	
-	for(i = 0; i < num_replicas; i++){
-		pthread_create(&pthreads[i], NULL, &run, (void*) i);
-	}
-	
-	pthread_barrier_wait(&barreira);
-
-
 	for(i = 0; i < num_replicas ; i++){
-		pthread_join(pthreads[i], NULL);
+		pthread_create(&pthreads[i], NULL, &run, (void*) i);	
 	}
 	
-	pthread_barrier_destroy(&barreira);
+	pthread_cond_broadcast(&pthreads);
 	
-	return 0;
+	sem_wait(&sema);
+	return x;
 }
 
 int main (int argc , char *argv[]){
