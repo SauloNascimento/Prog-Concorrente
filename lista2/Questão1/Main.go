0e5098b2dda3555fa7a6b8d43376f33809b2a7c3
tainahemmanuele <tainahemmanuele@gmail.com>
2019-06-18 16:58:26 -0300

    Inicio resolucao da questao 1, letra a. Letra a quase pronta

---
+++ lista2/Questão1/Main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+func request(done chan int) {
+	for {
+		select {
+		case <-done:
+			return
+		default:
+			r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			tempo := r.Intn(30) + 1
+			println(tempo)
+			time.Sleep(time.Duration(tempo) * time.Second)
+			done <- tempo
+		}
+	}
+}
+
+func gateway(num_request int) {
+	done := make(chan int)
+	for i := 0; i < num_request; i++ {
+		go request(done)
+	}
+	result := <- done
+	close(done)
+	println(result)
+
+}
+
+
+func main() {
+	e := os.Args[1]
+
+	num, _ := strconv.Atoi(e)
+	gateway(num)
+
+}
