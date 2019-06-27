467cc25e9d9b822fa2b4bb816d625084c8b074f6
tainahemmanuele <tainahemmanuele@gmail.com>
2019-06-27 13:18:53 -0300

    Refatoramento questão 1 letra b

--- lista2/Questão1/q1_b.go
+++ lista2/Questão1/q1_b.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
@@ -8,36 +9,39 @@ import (
 )
 
 var count int = 0
-var sum int  =0
+var sum int = 0
 
 func request(done chan int, num_request int) {
-	for {
-		if count == num_request {
-			done <- sum
-			return
-		} else {
-			r := rand.New(rand.NewSource(time.Now().UnixNano()))
-			tempo := r.Intn(30) + 1
-			time.Sleep(time.Duration(tempo) * time.Second)
-			count++
-			sum += tempo
-		}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	tempo := r.Intn(30) + 1
+	fmt.Println(tempo)
+	time.Sleep(time.Duration(tempo) * time.Second)
+	count++
+	sum += tempo
+	if (num_request == count){
+		done <- sum
 	}
+
 }
 
 func gateway(num_request int) {
 	done := make(chan int)
 	for i := 0; i < num_request; i++ {
-		go request(done,num_request)
+		select {
+		case <-done:
+			return
+		default:
+			go request(done,num_request)
+		}
 	}
-	result := <- done
+	result := <-done
 	close(done)
-	println(result)
+	fmt.Println(result)
 
 }
 
-
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	e := os.Args[1]
 	num, _ := strconv.Atoi(e)
 	gateway(num)
